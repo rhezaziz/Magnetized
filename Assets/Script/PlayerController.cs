0898a3207a74bbd8b9e0f9983a01b1d524abbf93
rhezaziz <rheza.khairullahaziz10@gmail.com>
2020-11-05 02:15:50 +0700

    first

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody2D rb2D;
+
+    public float pullForce = 100f;
+    public float rotateSpeed = 360f;
+    private GameObject closestTower;
+    private GameObject hookedTower;
+    private bool isPulled = false;
+    private UIController uiControl;
+    public GameObject Tower1, Tower2;
+
+    private AudioSource myAudio;
+
+    public float moveSpeed = 5f;
+    void Start()
+    {
+        rb2D = this.gameObject.GetComponent<Rigidbody2D>();
+        myAudio = this.gameObject.GetComponent<AudioSource>();
+        uiControl = GameObject.Find("Canvas").GetComponent<UIController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Move the object
+        rb2D.velocity = -transform.up * moveSpeed;
+
+        //Move the object
+        rb2D.velocity = -transform.up * moveSpeed;       
+       
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Debug.Log("kena");
+            //Hide game object
+            this.gameObject.SetActive(false);
+            uiControl.restartGame();
+            
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Goal")
+        {
+            Debug.Log("Levelclear!");
+            uiControl.endGame();
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Tower")
+        {
+            closestTower = collision.gameObject;
+
+            //Change tower color back to green as indicator
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.green;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isPulled) return;
+
+        if (collision.gameObject.tag == "Tower")
+        {
+            closestTower = null;
+            hookedTower = null;
+            //Change tower color back to normal
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+
+    public void klikDownTower()
+    {
+        Debug.Log("klik");
+        if (!isPulled)
+        {
+            if (closestTower != null && hookedTower == null)
+            {
+                hookedTower = closestTower;
+            }
+            if (hookedTower)
+            {
+                float distance = Vector2.Distance(transform.position, hookedTower.transform.position);
+
+                //Gravitation toward tower
+                Vector3 pullDirection = (hookedTower.transform.position - transform.position).normalized;
+                float newPullForce = Mathf.Clamp(pullForce / distance, 20, 50);
+                rb2D.AddForce(pullDirection * newPullForce);
+
+                //Angular velocity
+                rb2D.angularVelocity = -rotateSpeed / distance;
+                isPulled = true;
+            }
+        }
+
+    }
+
+
+    public void klikUpTower()
+    {
+        Debug.Log("up");
+        rb2D.angularVelocity = 0f;
+        isPulled = false;
+        hookedTower = null;
+    }
+
+}
---
+++ Assets/Script/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public PlayerController player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        // Runs by pressing the button
+        player.klikDownTower();
+    }
+
+
+    private void OnMouseUp()
+    {
+        // Runs when button is released
+        player.klikUpTower();
+    }
+}
